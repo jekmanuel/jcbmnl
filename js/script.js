c6ed91289d8993e88108a2db077f7ac6ef7f23d4
jekmanuel <jek.manuel01@gmail.com>
2025-03-24 12:41:54 +0800

    Files upload

---
+++ js/script.js
@@ -0,0 +1,48 @@
+// import 'owl.carousel/dist/assets/owl.carousel.css';
+// import 'owl.carousel';
+
+$(window).on("scroll", function () {
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop >= 80) {
+        $('body').addClass('fixed-header');
+    } else {
+        $('body').removeClass('fixed-header')
+    }
+});
+
+//Doc Ready
+$(document).ready(function () {
+
+    //Typed
+    new Typed('#type-it', {
+        strings: ['Designer', 'Developer', 'Freelancer'],
+        typeSpeed: 80,
+        loop: true
+    });
+
+    //Owl
+    $('.owl-carousel').owlCarousel({
+        dots: true,
+        loop: true,
+        margin: 30,
+        items: 2,
+        stagePadding: 2,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            900: {
+                items: 2
+            },
+        }
+    });
+
+    //scroll
+    $.scrollIt({
+        easing: 'linear',
+        topOffset: -70
+    });
+});
